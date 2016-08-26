6ef772207e16c45aa19bddaaeb72c7bd542b6713
BANGKOKAIR\kritawit <kritawitbunket@gmail.com>
2016-08-26 16:48:18 +0700

    the hibernate configuration and set properties values in 'application.properties' finaly add messages sources method for validation messages.

--- src/main/java/com/bangkokair/configuration/AppConfig.java
+++ src/main/java/com/bangkokair/configuration/AppConfig.java
@@ -1,7 +1,10 @@
 package com.bangkokair.configuration;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -32,6 +35,13 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setPrefix("/WEB-INF/views/");
 		viewResolver.setSuffix(".jsp");
 		registry.viewResolver(viewResolver);
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("messages");
+		return messageSource;
 	}
 
 }
